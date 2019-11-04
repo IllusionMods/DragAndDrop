5c1f20d17560c1fb327ac9d641fbc4ef1adec712
Keelhauled <keelhauledhs@gmail.com>
2019-11-04 18:11:40 +0200

    Add plugins

--- src/DragAndDrop.AISyoujyo/Properties/AssemblyInfo.cs
+++ src/DragAndDrop.AISyoujyo/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DragAndDrop")]
+[assembly: AssemblyTitle("DragAndDrop.AISyoujyo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DragAndDrop")]
+[assembly: AssemblyProduct("DragAndDrop.AISyoujyo")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("607ae753-9ecf-4e8e-b2c0-ec3640d19ce4")]
+[assembly: Guid("dac82a17-7fe0-478e-932d-4ef293a31a32")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(KeelPlugins.DragAndDrop.Version)]
