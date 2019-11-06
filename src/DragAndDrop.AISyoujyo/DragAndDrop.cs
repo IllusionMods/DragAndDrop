3cf7ddab720c1b589b243c999c5fa3feac1463d1
Mantas <syenergyy@gmail.com>
2019-11-06 15:02:53 +0200

    Added support for AIS Studio and housing (#1)
    * Added support for AIS Studio
    
    * Added housing support for AIS
    Borrowed some code from https://github.com/YumeMienai/KoikatuDragAndDrop/commit/322eec8bc3fd9a910a319c4e9d24a7e7409adce9
    
    * Added warnings when loading studio scenes, housing in incorrect scenes

--- src/DragAndDrop.AISyoujyo/DragAndDrop.cs
+++ src/DragAndDrop.AISyoujyo/DragAndDrop.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Manager;
 
 namespace DragAndDrop
 {
     [BepInPlugin(GUID, PluginName, Version)]
     public class DragAndDrop : DragAndDropCore
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
 
         private static readonly byte[] CharaToken = Encoding.UTF8.GetBytes("【AIS_Chara】");
         private static readonly byte[] SexToken = Encoding.UTF8.GetBytes("sex");
-
+        private static readonly byte[] StudioToken = Encoding.UTF8.GetBytes("KStudio");
+        private static readonly byte[] CoordinateToken = Encoding.UTF8.GetBytes("AIS_Clothes");
+        private static readonly byte[] PoseToken = Encoding.UTF8.GetBytes("【pose】");
+        private static readonly byte[] HouseToken = Encoding.UTF8.GetBytes("【AIS_Housing】");
+        
         internal override void OnFiles(List<string> aFiles, POINT aPos)
         {
             var goodFiles = aFiles.Where(x =>
@@ -31,15 +36,40 @@ namespace DragAndDrop
             var cardHandler = CardHandlerMethods.GetActiveCardHandler();
             if(cardHandler != null)
             {
+                bool inStudio = Scene.Instance && Scene.Instance.NowSceneNames.Any(x => x == "Studio");
+                bool inHousing = Scene.Instance && Scene.Instance.NowSceneNames.Any(x => x == "Map") && FindObjectsOfType<Housing.UICtrl>().Any(i => i.IsInit);
+                
                 foreach(var file in goodFiles)
                 {
                     var bytes = File.ReadAllBytes(file);
 
-                    if(BoyerMoore.ContainsSequence(bytes, CharaToken))
+                    if(BoyerMoore.ContainsSequence(bytes, StudioToken))
+                    {
+                        if(!inStudio)
+                            Logger.LogMessage("Can't load studio scene file in current scene");
+                        else
+                            cardHandler.Scene_Load(file, aPos);
+                    }
+                    else if(BoyerMoore.ContainsSequence(bytes, CharaToken))
                     {
                         var index = new BoyerMoore(SexToken).Search(bytes).First();
                         var sex = bytes[index + SexToken.Length];
                         cardHandler.Character_Load(file, aPos, sex);
+                    }
+                    else if(BoyerMoore.ContainsSequence(bytes, CoordinateToken))
+                    {
+                        cardHandler.Coordinate_Load(file, aPos);
+                    }
+                    else if (BoyerMoore.ContainsSequence(bytes, PoseToken))
+                    {
+                        cardHandler.PoseData_Load(file, aPos);
+                    }
+                    else if(BoyerMoore.ContainsSequence(bytes, HouseToken))
+                    {
+                        if(!inHousing)
+                            Logger.LogMessage("Can't load housing file in current scene");
+                        else
+                            cardHandler.HouseData_Load(file, Logger);
                     }
                     else
                     {
---
+++ src/DragAndDrop.AISyoujyo/HousingHandler.cs
@@ -0,0 +1,37 @@
+using Housing;
+using System.Linq;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace DragAndDrop
+{
+    internal class HousingHandler : CardHandlerMethods
+    {
+        public override bool Condition => Singleton<Manager.Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "Map") && Object.FindObjectsOfType<UICtrl>().Any(i => i.IsInit);
+        
+        public override void HouseData_Load(string path, ManualLogSource Logger)
+        {
+            var craftInfo = CraftInfo.LoadStatic(path);
+            var housingID = Singleton<CraftScene>.Instance.HousingID;
+            
+            if (Singleton<Manager.Housing>.Instance.dicAreaInfo.TryGetValue(housingID, out var areaInfo))
+            {
+                if (Singleton<Manager.Housing>.Instance.dicAreaSizeInfo.TryGetValue(areaInfo.size, out var areaSizeInfo))
+                {
+                    if (areaSizeInfo.compatibility.Contains(Singleton<Manager.Housing>.Instance.GetSizeType(craftInfo.AreaNo)))
+                    {
+                        Singleton<Selection>.Instance.SetSelectObjects(null);
+                        Singleton<UndoRedoManager>.Instance.Clear();
+                        Singleton<Manager.Housing>.Instance.Load(path, true, true);
+                        Singleton<Manager.Housing>.Instance.CheckOverlap();
+                        Object.FindObjectsOfType<UICtrl>().First(i => i.IsInit).ListUICtrl.UpdateUI();
+                    }
+                    else
+                    {
+                        Logger.LogMessage("Incorrect housing location");
+                    }
+                }
+            }
+        }
+    }
+}
