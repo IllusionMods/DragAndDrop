42ab81e49190e04dc7b1d336d0c4612696aa28d7
YumeMienai <yyut@drmail.net>
2020-01-11 14:52:28 +0800

    Use LoadAsync reduce the load time which is same as the new process in original game

--- src/DragAndDrop.AISyoujyo/HousingHandler.cs
+++ src/DragAndDrop.AISyoujyo/HousingHandler.cs
@@ -1,6 +1,8 @@
 using Housing;
 using System.Linq;
+using AIProject.Scene;
 using BepInEx.Logging;
+using Illusion.Extensions;
 using UnityEngine;
 using Manager;
 
@@ -9,23 +11,34 @@ namespace DragAndDrop
     internal class HousingHandler : CardHandlerMethods
     {
         public override bool Condition => Singleton<Scene>.Instance.NowSceneNames.Any(sceneName => sceneName == "Map") && Object.FindObjectsOfType<UICtrl>().Any(i => i.IsInit);
-        
+
         public override void HouseData_Load(string path, ManualLogSource Logger)
         {
             var craftInfo = CraftInfo.LoadStatic(path);
-            var housingID = Singleton<CraftScene>.Instance.HousingID;
-            
+            var craftScene = Singleton<CraftScene>.Instance;
+            var housingID = craftScene.HousingID;
+
             if (Singleton<Manager.Housing>.Instance.dicAreaInfo.TryGetValue(housingID, out var areaInfo))
             {
                 if (Singleton<Manager.Housing>.Instance.dicAreaSizeInfo.TryGetValue(areaInfo.size, out var areaSizeInfo))
                 {
                     if (areaSizeInfo.compatibility.Contains(Singleton<Manager.Housing>.Instance.GetSizeType(craftInfo.AreaNo)))
                     {
-                        Singleton<Selection>.Instance.SetSelectObjects(null);
-                        Singleton<UndoRedoManager>.Instance.Clear();
-                        Singleton<Manager.Housing>.Instance.Load(path, true, true);
-                        Singleton<Manager.Housing>.Instance.CheckOverlap();
-                        Object.FindObjectsOfType<UICtrl>().First(i => i.IsInit).ListUICtrl.UpdateUI();
+                        ConfirmScene.Sentence = "¥Ç©`¥¿¤òÕiÞz¤ß¤Þ¤¹¤«£¿\n" + "¥»¥Ã¥È¤µ¤ì¤¿¥¢¥¤¥Æ¥à¤ÏÏ÷³ý¤µ¤ì¤Þ¤¹¡£".Coloring("#DE4529FF").Size(24);
+                        ConfirmScene.OnClickedYes = () =>
+                        {
+                            Singleton<Selection>.Instance.SetSelectObjects(null);
+                            craftScene.UICtrl.ListUICtrl.ClearList();
+                            Singleton<UndoRedoManager>.Instance.Clear();
+                            craftScene.WorkingUICtrl.Visible = true;
+                            Singleton<Manager.Housing>.Instance.LoadAsync(path, obj =>
+                            {
+                                craftScene.UICtrl.ListUICtrl.UpdateUI();
+                                craftScene.WorkingUICtrl.Visible = false;
+                            });
+                        };
+                        ConfirmScene.OnClickedNo = () => { };
+                        Singleton<Game>.Instance.LoadDialog();
                     }
                     else
                     {
