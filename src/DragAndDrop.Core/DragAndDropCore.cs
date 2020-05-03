bf74aec94de4969e6928e0e67e0eb50ec9fe53da
ManlyMarco <dummyaccx@gmail.com>
2020-05-03 18:54:08 +0200

    Version up to 1.2

--- src/DragAndDrop.Core/DragAndDropCore.cs
+++ src/DragAndDrop.Core/DragAndDropCore.cs
@@ -9,7 +9,7 @@ namespace DragAndDrop
     {
         public const string PluginName = "Drag & Drop";
         public const string GUID = "keelhauled.draganddrop";
-        public const string Version = "1.1.1";
+        public const string Version = "1.2";
         internal static new ManualLogSource Logger;
 
         private UnityDragAndDropHook hook;
