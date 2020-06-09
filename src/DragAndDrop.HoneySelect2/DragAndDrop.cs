7f650244f1dbca4111aeeb41c261007f7b4008c7
ManlyMarco <dummyaccx@gmail.com>
2020-06-10 00:06:20 +0200

    Added hs2 port

---
+++ src/DragAndDrop.HoneySelect2/DragAndDrop.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DragAndDrop
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class DragAndDrop : DragAndDropCore
+    {
+        private static readonly byte[] CharaToken = Encoding.UTF8.GetBytes("【AIS_Chara】");
+        private static readonly byte[] SexToken = Encoding.UTF8.GetBytes("sex");
+        private static readonly byte[] StudioOldToken = Encoding.UTF8.GetBytes("KStudio"); // Compatibility with older scenes created before the 11-08 update
+        private static readonly byte[] StudioNewToken = Encoding.UTF8.GetBytes("StudioNEOV2");
+        private static readonly byte[] CoordinateToken = Encoding.UTF8.GetBytes("AIS_Clothes");
+        private static readonly byte[] PoseToken = Encoding.UTF8.GetBytes("【pose】");
+        
+        internal override void OnFiles(List<string> aFiles, POINT aPos)
+        {
+            var goodFiles = aFiles.Where(x =>
+            {
+                var ext = Path.GetExtension(x).ToLower();
+                return ext == ".png";
+            });
+
+            if(goodFiles.Count() == 0)
+            {
+                Logger.LogMessage("No files to handle");
+                return;
+            }
+
+            if(CardHandlerMethods.GetActiveCardHandler(out var cardHandler))
+            {
+                foreach(var file in goodFiles)
+                {
+                    var bytes = File.ReadAllBytes(file);
+
+                    if(BoyerMoore.ContainsSequence(bytes, StudioNewToken) || BoyerMoore.ContainsSequence(bytes, StudioOldToken))
+                    {
+                        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                            cardHandler.Scene_Import(file, aPos);
+                        else
+                            cardHandler.Scene_Load(file, aPos);
+                    }
+                    else if(BoyerMoore.ContainsSequence(bytes, CharaToken))
+                    {
+                        var index = new BoyerMoore(SexToken).Search(bytes).First();
+                        var sex = bytes[index + SexToken.Length];
+                        cardHandler.Character_Load(file, aPos, sex);
+                    }
+                    else if(BoyerMoore.ContainsSequence(bytes, CoordinateToken))
+                    {
+                        cardHandler.Coordinate_Load(file, aPos);
+                    }
+                    else if (BoyerMoore.ContainsSequence(bytes, PoseToken))
+                    {
+                        cardHandler.PoseData_Load(file, aPos);
+                    }
+                    else
+                    {
+                        Logger.LogMessage("This file does not contain any HS2 related data");
+                    }
+                }
+            }
+            else
+            {
+                Logger.LogMessage("No handler found for this scene");
+            }
+        }
+    }
+}
---
+++ src/DragAndDrop.HoneySelect2/MakerHandler.cs
@@ -0,0 +1,40 @@
+﻿using CharaCustom;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DragAndDrop
+{
+    internal class MakerHandler : CardHandlerMethods
+    {
+        public override bool Condition => GameObject.FindObjectOfType<CharaCustom.CharaCustom>();
+
+        public override void Character_Load(string path, POINT pos, byte sex)
+        {
+            var cvsCharaLoad = GameObject.FindObjectOfType<CvsO_CharaLoad>();
+            var traverse = Traverse.Create(cvsCharaLoad);
+            var charaLoadWin = traverse.Field("charaLoadWin");
+            var tglLoadOption = charaLoadWin.Field("tglLoadOption").GetValue<Toggle[]>();
+            int num = 0;
+
+            if(tglLoadOption != null)
+            {
+                if(tglLoadOption[0].isOn) num |= 1;
+                if(tglLoadOption[1].isOn) num |= 2;
+                if(tglLoadOption[2].isOn) num |= 4;
+                if(tglLoadOption[3].isOn) num |= 8;
+                if(tglLoadOption[4].isOn) num |= 16;
+            }
+
+            charaLoadWin.GetValue<CustomCharaWindow>().onClick03.Invoke(new CustomCharaFileInfo { FullPath = path }, num);
+        }
+
+        public override void Coordinate_Load(string path, POINT pos)
+        {
+            var cvsClothesLoad = GameObject.FindObjectOfType<CvsC_ClothesLoad>();
+            var traverse = Traverse.Create(cvsClothesLoad);
+            var clothesLoadWin = traverse.Field("clothesLoadWin");
+            clothesLoadWin.GetValue<CustomClothesWindow>().onClick03.Invoke(new CustomClothesFileInfo { FullPath = path });
+        }
+    }
+}
